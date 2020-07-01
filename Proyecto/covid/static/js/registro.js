75c6be1fc2c127796bf3cf14a14d758f7d574234
Aldo Muñoz <aldo@aldored.com>
2020-06-30 20:58:52 -0400

    Modificaciones

--- Proyecto/covid/static/js/registro.js
+++ Proyecto/covid/static/js/registro.js
@@ -1,6 +1,17 @@
 $("#registro .1-1 .siguiente").click(function(){
-    $("#registro .1-1").addClass('noDisplay');
-    $("#registro .1-2").removeClass('noDisplay');
+
+    var nombre = $("#registro #nombre").val();
+    var apellido = $("#registro #apellido").val();
+    var email = $("#registro #email").val();
+    var numero = $("#registro #numero").val();
+
+    if(nombre != "" && apellido != "" && email != "" && numero != ""){
+        $("#registro .1-1").addClass('noDisplay');
+        $("#registro .1-2").removeClass('noDisplay');
+    }else{
+        alert("Por favor, rellenar todos los campos");
+    }
+
 })
 
 $("#registro .1-2 .volver").click(function(){
@@ -10,4 +21,12 @@ $("#registro .1-2 .volver").click(function(){
 
 $("#registro .1-2 .siguiente").click(function(){
     alert("Registro")
+})
+
+$(".info").mouseover(function(){
+    $(".info div").removeClass("noDisplay");
+})
+
+$(".info").mouseout(function(){
+    $(".info div").addClass("noDisplay");
 })
--- Proyecto/covid/views.py
+++ Proyecto/covid/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
+    if(request.method == 'POST'):
+        print("1")
     return render(request, "index.html")
 
 def ingresar(request):
